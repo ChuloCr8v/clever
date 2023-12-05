5e73564011bf04071a1ba7e544aa434f1cf5f6a4
chulocr8v <nkematu5@gmail.com>
2023-12-05 23:55:37 +0100

    updated contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,10 +1,10 @@
 "use client";
 
-import React from "react";
-import FooterButton from "../components/FooterButton";
 import { AnimatePresence, motion } from "framer";
-import PageTitle from "../components/PageTitle";
 import { FaArrowRight } from "react-icons/fa";
+import FooterButton from "../components/FooterButton";
+import PageTitle from "../components/PageTitle";
+import Button from "../components/Button";
 
 type Props = {};
 
@@ -13,9 +13,14 @@ const Contact = (props: Props) => {
     {
       title: "Mail",
       url: "chulocr8v@gmail.com",
+      name: "chulocr8v",
     },
     {
-      title: "Social Media",
+      title: (
+        <p className="">
+          Social<span className="block text-2xl">Medias</span>
+        </p>
+      ),
       subMenu: [
         { url: "chulocr8v@gmail.com", title: "Facebook" },
         { url: "chulocr8v@gmail.com", title: "Instagram" },
@@ -25,6 +30,26 @@ const Contact = (props: Props) => {
       ],
     },
   ];
+
+  const inputClassName =
+    "bg-transparent border dark:border-gray-500 border-gray-400 rounded w-full px-3 py-2 outline-none hover:outline-blue-200 dark:hover:outline-primaryRed hover:border-primaryBlue dark:hover:border-primaryRed duration-200";
+  const formGroupClassName = "flex flex-col items-start gap-2 w-full";
+  const labelClassName = "capitalize font-semibold";
+
+  const FormGroup = (props: { label: string; placeholder: string }) => {
+    return (
+      <div className={formGroupClassName}>
+        <label htmlFor="" className={labelClassName}>
+          {props.label}
+        </label>
+        <input
+          type="text"
+          placeholder={props.placeholder}
+          className={inputClassName}
+        />
+      </div>
+    );
+  };
 
   return (
     <AnimatePresence>
@@ -39,22 +64,22 @@ const Contact = (props: Props) => {
           <PageTitle title={"Contact"} />
           <div className="flex flex-col gap-6 mt-12 w-full">
             {contacts.map((c, index) => (
-              <div className="flex justify-between items-start" key={index}>
-                <p className="font-semibold text-xl"> {c.title}</p>
+              <div className="grid grid-cols-2" key={index}>
+                <p className="font-semibold text-xl "> {c.title}</p>
                 {!c.subMenu ? (
                   <a
                     href="mailto:email@example.com"
-                    className=" flex items-center gap-2 group hover:text-primaryBlue dark:hover:text-primaryRed"
+                    className="flex items-center gap-2 group hover:text-primaryBlue dark:hover:text-primaryRed capitalize pl-16"
                   >
                     <FaArrowRight className="-rotate-45 group-hover:rotate-0 duration-200" />
-                    {c.url}
+                    {c.name}
                   </a>
                 ) : (
-                  <div className="flex flex-col items-start">
+                  <div className="flex flex-col items-start ">
                     {c?.subMenu?.map((s, index) => (
                       <a
                         href={s.url}
-                        className=" flex items-center gap-2 group hover:text-primaryBlue dark:hover:text-primaryRed"
+                        className="flex items-center gap-2 group hover:text-primaryBlue dark:hover:text-primaryRed pl-16"
                         key={index}
                       >
                         <FaArrowRight className="-rotate-45 group-hover:rotate-0 duration-200" />
@@ -66,14 +91,44 @@ const Contact = (props: Props) => {
               </div>
             ))}
           </div>
+          <div className="form mt-24 border dark:border-gray-500 border-gray-400 rounded p-3 pb-12">
+            <form
+              name="contact"
+              method="POST"
+              data-netlify="true"
+              className="flex flex-col items-start gap-4 w-full"
+            >
+              <div className="text-center w-full py-12 border dark:border-gray-500 border-gray-400 rounded">
+                <p className="text-3xl font-semibold">Message Me</p>
+                <p className=" mt-1 text-gray-500 dark:text-gray-400">
+                  Patiently waiting....
+                </p>
+              </div>
+
+              <FormGroup label={"name"} placeholder={"Enter name here"} />
+              <FormGroup label={"email"} placeholder={"Enter email here"} />
+
+              <div className={formGroupClassName}>
+                <label htmlFor="" className={labelClassName}>
+                  Message
+                </label>
+                <textarea
+                  className={inputClassName}
+                  placeholder="Write me a message"
+                  cols={12}
+                />
+              </div>
+              <Button title={"Send"} className="mt-4" />
+            </form>
+          </div>
         </motion.section>
       </div>
       <FooterButton
-        prevTitle={"About Me"}
-        nextTitle={"Contact"}
+        prevTitle={"Projects"}
+        nextTitle={"Resume"}
         icon={false}
-        nextLink="/contact"
-        prevLink="/about"
+        nextLink="/resume"
+        prevLink="/projects"
       />
     </AnimatePresence>
   );
