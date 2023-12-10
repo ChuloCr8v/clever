d8d67cd5e032e0418b03e45d43c5892ea9408650
chulocr8v <nkematu5@gmail.com>
2023-12-10 16:22:16 +0100

    u

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -22,9 +22,9 @@ const Hero = (props: Props) => {
         </p>
         <p className="text-base md:text-2xl mt-1 leading-none">
           <span className=" text-primaryBlue pr-2">Frontend Developer</span>
-          <span className="text-primaryRed border-l border-gray-500 pl-2">
+          {/* <span className="text-primaryRed border-l border-gray-500 pl-2">
             TypeScript
-          </span>
+          </span> */}
         </p>
         <div className="stacks flex flex-wrap justify-center items-center gap-2 mt-4 px-4">
           {stacks.map((s, index) => (
